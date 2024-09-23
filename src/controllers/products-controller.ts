7afb2c552351a9be9701eac5e31107f86ca4af28
Bernardo <beefreguglia@gmail.com>
2024-09-23 14:01:24 -0300

    fetaure: validating data of product with zod

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -8,6 +9,21 @@ class ProductController {
       });
     } catch (error) {
       next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      return response.status(201).json({ name, price })
+    } catch (error) {
+      next(error)
     }
   }
 }
