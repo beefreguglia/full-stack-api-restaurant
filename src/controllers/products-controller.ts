7afb045652411faaa80afc2ff478c1fcbaada4a8
Bernardo <beefreguglia@gmail.com>
2024-09-23 17:12:35 -0300

    fetaure: create delete product with id

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 
 import { knex } from '@/database/knex';
+import { AppError } from '@/utils/AppError';
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -48,9 +49,43 @@ class ProductController {
 
       const { name, price } = bodySchema.parse(request.body);
 
+      const product = await knex<ProductRepository>("product")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!product) {
+        throw new AppError("Product not found!")
+      }
+      
       await knex<ProductRepository>("products")
         .update({ name, price, updated_at: knex.fn.now() })
         .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number."})
+        .parse(request.params.id);
+      
+      const product = await knex<ProductRepository>("product")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!product) {
+        throw new AppError("Product not found!")
+      }
+
+      await knex<ProductRepository>("products").delete().where({ id });
 
       return response.json();
     } catch (error) {
